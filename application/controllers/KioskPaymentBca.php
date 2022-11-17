f4d41df0b7dc0f219bbf65d7a7042cda07ffc2c7
reynave <felix.renaldi@gmail.com>
2022-11-17 18:48:18 +0700

    update

--- application/controllers/KioskPaymentBca.php
+++ application/controllers/KioskPaymentBca.php
@@ -14,7 +14,7 @@ class kioskPaymentBca extends CI_Controller
         header('Access-Control-Allow-Methods: GET, POST, PUT');
         header('Content-Type: application/json');
         date_default_timezone_set('Asia/Jakarta');
-
+        /*
         if (!$this->model->checkDeviceObj()) {
             //  echo $this->model->error("Error auth");
             //  exit;
@@ -22,7 +22,7 @@ class kioskPaymentBca extends CI_Controller
             $getDeviceObj =  $this->model->getDeviceObj();
             $this->terminalId =  $getDeviceObj['terminalId'];
             $this->storeOutlesId =  $getDeviceObj['storeOutlesId'];
-        }
+        }*/
     }
 
     function index()
@@ -117,15 +117,57 @@ class kioskPaymentBca extends CI_Controller
         echo json_encode($data);
     }
 
+    /*
+    BCA ERC 
+    */
+    function fnBcaEcr32($reffNo)
+    {
+       
+        if ($reffNo) { 
+            $data = []; 
+            $erc_STX            = "02";
+            $erc_messageLenght  = "0150";
+            $erc_version        = "02"; 
+            $erc_dat2 = "32000000000000000000000000                       00000000000000  N00000            232116000753                                                      ";
+            $erc_data = "32000000000000000000000000                       00000000000000  N00000            ".$reffNo."                                                      ";
+            $erc_ETX           = "03";
+
+
+            $msg_length =  strlen($erc_version .$erc_data);
+            $erc_message =  ($erc_data);
+
+            $hex  = $erc_STX .
+                $erc_messageLenght .
+                $erc_version .
+                bin2hex($erc_data) . 
+                $erc_ETX .
+                $this->ecr->fnXor($erc_message);
+
+            $data = array(
+                "msg" => $erc_message,
+                "length" => $msg_length,
+                "hex" =>  $hex,
+                "xor" =>  $this->ecr->fnXor($erc_message),
+            
+            );   
+        } else {
+            $data = array(
+                "error" => true,
+            );
+        }
+       return ($data);
+    }
     function fnQrisCheck()
     {
         $post =   json_decode(file_get_contents('php://input'), true);
 
         if ($post) {
             $id = $this->model->select("id", "cso1_paymentBcaQris", "presence = 1 and  kioskUuid = '" . $post['kioskUuid'] . "' ");
+            $reffNo = $this->model->select("reffNo", "cso1_paymentBcaQris", "presence = 1 and  kioskUuid = '" . $post['kioskUuid'] . "' ");
             $data = array(
                 "data" => $id ? $this->model->sql("select * from cso1_paymentBcaQris where id= $id ")[0] : false,
                 "error" => false,
+                "hex" => self::fnBcaEcr32( $reffNo ),
             );
         } else {
             $data = array(
@@ -135,34 +177,24 @@ class kioskPaymentBca extends CI_Controller
         echo json_encode($data);
     }
 
-    function fnQrisInsert()
+    function fnCheck()
     {
-        $post =   json_decode(file_get_contents('php://input'), true);
+        $kioskUuid = $this->input->get("kioskUuid");
+        $post = $this->model->sql("select * from cso1_paymentBcaQris where kioskUuid = '$kioskUuid' ")[0];
 
-        if ($post) {
-            $insert = array(
-                "kioskUuid" =>  $post['kioskUuid'],
-                "reffNo" =>  $post['hex'],
-                "hex" =>  $post['hex'],
-                "asciiString"  =>  $post['hex'],
-                "inputDate"  => time(),
-                "updateDate"  => time(),
-            );
-            $this->db->insert($insert);
+        // print_r($post );
+        // $refNo = (string)preg_replace('/\s\s+/', ' ', $post['asciiString']);
+        // echo $refNo;
+        // $refNo = (explode(" ", $refNo));
+        // print_r( $refNo);
+        // $refNo = $refNo[1];
+        // echo substr( $refNo, 2, 12);
 
-            $data = array(
-                "insert" =>  $insert,
-                "error" => false,
-            );
-        } else {
-            $data = array(
-                "error" => true,
-            );
-        }
-        echo json_encode($data);
+        $refNo = (string)preg_replace('/\s\s+/', ' ', $post['asciiString']);
+
+        $refNo = substr($refNo, 0, 45);
+        echo  'hasil ' . $refNo;
     }
-
-
 
     function fnBcaCashDEL()
     {
@@ -233,6 +265,71 @@ class kioskPaymentBca extends CI_Controller
                 "post" => $post,
                 "data" => $data,
                 "error" => false,
+            );
+        } else {
+            $data = array(
+                "error" => true,
+            );
+        }
+        echo json_encode($data);
+    }
+
+
+    function fnSaveLog()
+    {
+        $post =   json_decode(file_get_contents('php://input'), true);
+        $error = true;
+        if ($post && $post['data']['respCode']) {
+            $insert = array(
+                "respCode"  => $post['data']['respCode'],
+                "kioskUuid" =>  $post['kioskUuid'],
+                "transType" =>  $post['data']['transType'],
+                "hex"       => $post['data']['hex'],
+                "inputDate" => time(),
+            );
+
+            $this->db->insert("cso1_paymentBcaLog", $insert);
+            $data = array(
+                "error" => false,
+                "insert" => $insert,
+            );
+        } else {
+            $data = array(
+                "error" => true,
+            );
+        }
+        echo json_encode($data);
+    }
+    function fnQrisInsert()
+    {
+        $post =   json_decode(file_get_contents('php://input'), true);
+        $error = true;
+        if ($post && $post['data']['respCode']) {
+
+            $this->db->delete("cso1_paymentBcaQris", "kioskUuid = '" . $post['kioskUuid'] . "'");
+
+            $refNo = preg_replace('/\s\s+/', ' ', $post['data']['ascii']);
+            $refNo  = substr($refNo, 33, 12);
+            $insert = array(
+                "kioskUuid" =>  $post['kioskUuid'],
+                "reffNo" =>   $refNo,
+                "hex" =>  $post['data']['hex'],
+                "asciiString"  =>  $post['data']['ascii'],
+                "inputDate"  => time(),
+                "updateDate"  => time(),
+            );
+            $id = $this->model->select("id", "cso1_paymentBcaQris", "kioskUuid = '" . $post['kioskUuid'] . "'  and reffNo = '" . $refNo . "'");
+            if (!$id) {
+                $this->db->insert("cso1_paymentBcaQris", $insert);
+            } else {
+                $insert = false;
+            }
+
+            $data = array(
+                "error" => false,
+                "insert" => $insert,
+                "refNo" => $refNo,
+                "id" =>  $id,
             );
         } else {
             $data = array(
